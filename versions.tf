d87ba0494ee925afc4dc22c07af83e58fa3c7135
Siddharth Suresh <siddh.suresh@gmail.com>
2026-04-10 00:10:49 +0530

    Add cloud configuration for Terraform with organization and workspace

--- versions.tf
+++ versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  cloud {
+    organization = "siddhsuresh_dev"
+
+    workspaces {
+      name = "dev"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
